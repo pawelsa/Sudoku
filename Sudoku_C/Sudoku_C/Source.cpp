5f2bba3828073ce250d3599c5b59fe0e43c9e826
pawelsa <pawel.sapek97@gmail.com>
2016-12-21 17:08:45 +0100

    Update Source.cpp

--- Sudoku_C/Sudoku_C/Source.cpp
+++ Sudoku_C/Sudoku_C/Source.cpp
@@ -1,165 +1,162 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-
+#include <windows.h>
 
 #define sud 9
 int sudoku[sud][sud];
-int wartosc;
+int sudokubl[sud][sud];
+int value;
 int x, y;
-
 
 void tablica(int sudoku[sud][sud]) {
 
+	//Draw 3 rows
+	printf(" Y |     \t\tX\n   |");
+	for (int i = 1; i < 10; i++) {
+		printf("%3.d", i);
 
+		while ((i == 3) || (i == 6)) {
+			printf(" |");
+			break;
+		}
+	}
+	printf("\n");
+	for (int k = 0; k < 18; k++)
+		printf(" -");
+	printf("\n");
+
+	//Draw full table
 	for (int i = 0; i < sud; i++) {
+		printf(" %d |", i + 1);
 		for (int j = 0; j < sud; j++) {
-			printf("%3.d", sudoku[i][j]);	  //liczby
-			while ((j == 2) || (j == 5)) {		//odstêpy
-				printf("|");
-				break;
-			}	//pêtla while
-		}	 //pêtla for
+			printf("%3.d", sudoku[i][j]);	  //values
 
+			while ((j == 2) || (j == 5)) {		//spaces
+				printf(" |");
+				break;
+			}	//loop while
+		}	 //loop for
 
 		printf("\n");
 
-
+		//Horizontal spaces
 		while ((i == 2) || (i == 5)) {
-			for (int k = 0; k < 15; k++)
+			for (int k = 0; k < 18; k++)
 				printf(" -");
 			printf("\n");
 			break;
-		}//pêtla while ^
+		}//loop while ^
 
-
-	}//1 pentla
-
+	}//1st loop
 
 }
 
+int sprawdzanie(int sudoku[sud][sud], int value, int x, int y, int sudokubl[sud][sud]) {
 
-int sprawdzanie(int sudoku[sud][sud], int wartosc, int x, int y) {
+	int condition = 0;//if condition = 1, value can't be signed
 
-
-	//wprowadzanie i sprawdzanie miejsca zmiany
+					  //Prepares values for checking 3x3 squares
 	int Ywar, Xwar;
-
-
-	int warunek = 0;
-
-
 	Ywar = Xwar = 0;
-
 
 	if ((y >= 4) && (y <= 6))
 		Ywar = 3;
 	else if (y >= 7)
 		Ywar = 6;
 
-
 	if ((x >= 4) && (x <= 6))
 		Xwar = 3;
 	else if (x >= 7)
 		Xwar = 6;
 
-
+	//Check if values may be signed without corrupion of sudoku rules in horizontal and vertical axis
 	for (int i = 0; i < 9; i++) {
-		if ((wartosc == sudoku[y - 1][i]) || (wartosc == sudoku[i][x - 1])) {
-			warunek = 1;
+		if ((value == sudoku[y - 1][i]) || (value == sudoku[i][x - 1])) {
+			condition = 1;
 			break;
 		}
 	}
 
-
+	//Checks 3x3 squares
 	for (int k = Ywar; k < (Ywar + 3); k++)		//3-6	6 - 9
 		for (int j = Xwar; j < (Xwar + 3); j++)
-			if (sudoku[k][j] == wartosc)
-				warunek = 1;
+			if (sudoku[k][j] == value)
+				condition = 1;
 
-
-	if (warunek == 0){
-		sudoku[y - 1][x - 1] = wartosc;
-		printf("\nDobrze ! Jedziemy dalej\n\n");
+	if (condition == 0) {
+		sudoku[y - 1][x - 1] = value;
+		printf("\nGood ! Go ahead\n\n");
 	}
-	else{
-		printf("Nie mozna tego dodac, poniewaz bedzie to lamanie zasad sudoku !!\nWartosci nie moga sie powtarzac w obu osiach !!\nWracamy do poczatku zmiany !!\n");
+	else {
+		printf("NOPE ! You can't sign here, because it breaks sudoku rules! \n");
 	}
-	return warunek;
+	return condition;
 }
 
+int finalcheck(int sudoku[sud][sud], int sudokubl[sud][sud]) {
 
-int main(){
+	int condition = 0;
 
+	//Checks if all values are different than 0
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			if (sudoku[i][j] != 0);
+			else
+				condition = 1;
+		}
+	}
+	return condition;
+}
 
-	for (int i = 0; i < 10; i++)
-		for (int j = 0; j < 10; j++)
-			sudoku[i][j] = 0;
+void save(int sudoku[sud][sud]) {
 
+	FILE *save;
+	save = fopen("sudoku.txt", "w");
 
-	sudoku[0][1] = 5;
-	sudoku[0][2] = 3;
-	sudoku[0][3] = 2;
-	sudoku[0][5] = 7;
-	sudoku[0][8] = 8;
-	sudoku[1][0] = 6;
-	sudoku[1][2] = 1;
-	sudoku[1][3] = 5;
-	sudoku[1][8] = 2;
-	sudoku[2][0] = 2;
-	sudoku[2][3] = 9;
-	sudoku[2][4] = 1;
-	sudoku[2][5] = 3;
-	sudoku[2][7] = 5;
-	sudoku[3][0] = 7;
-	sudoku[3][1] = 1;
-	sudoku[3][2] = 4;
-	sudoku[3][3] = 6;
-	sudoku[3][4] = 9;
-	sudoku[3][5] = 2;
-	sudoku[4][1] = 2;
-	sudoku[4][7] = 6;
-	sudoku[5][3] = 4;
-	sudoku[5][4] = 5;
-	sudoku[5][5] = 1;
-	sudoku[5][6] = 2;
-	sudoku[5][7] = 9;
-	sudoku[5][8] = 7;
-	sudoku[6][1] = 6;
-	sudoku[6][3] = 3;
-	sudoku[6][4] = 2;
-	sudoku[6][5] = 5;
-	sudoku[6][8] = 9;
-	sudoku[7][0] = 1;
-	sudoku[7][5] = 6;
-	sudoku[7][6] = 3;
-	sudoku[7][8] = 4;
-	sudoku[8][0] = 8;
-	sudoku[8][3] = 1;
-	sudoku[8][5] = 9;
-	sudoku[8][6] = 6;
-	sudoku[8][7] = 7;
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			fprintf(save, "%d ", sudoku[i][j]);
+		}
+		fprintf(save, "\n");
+	}
+	fclose(save);
+}
 
-	do{
-		do{
-			tablica(sudoku);
+void read(int sudoku[sud][sud], int sudokubl[sud][sud]) {
+
+	FILE *save;
+	FILE *saveblock;
+	save = fopen("sudoku.txt", "r");
+	saveblock = fopen("sudokublock.txt", "r");
+
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			fscanf(save, "%d ", &sudoku[i][j]);
+			fscanf(saveblock, "%d ", &sudokubl[i][j]);
+		}
+	}
+	fclose(save);
+	fclose(saveblock);
+}
+
+int main() {
+
+	do {
+		read(sudoku, sudokubl);//Reads a file
+		tablica(sudoku);//Draw table
+		do {
 			do {
-				printf("\nGdzie chcesz zmienic pole ?\n");
-				printf("Podaj x, y : ");
+				printf("\n Which box do you want to change ?\n");
+				printf(" Enter x, y : ");
 				scanf("%d %d", &x, &y);
-			} while (((x < 1) && (x > 9)) && ((y < 1) && (y > 9)));
-			do {
-				printf("\nPodaj wartosc <1-9> : ");
-				scanf("%d", &wartosc);
-			} while ((wartosc < 1) && (wartosc > 9));
-		} while ((sprawdzanie(sudoku, wartosc, x, y)) != 1);
-	} while ((sprawdzanie(sudoku, wartosc, x, y)) == 0);
+			} while (((x < 1) && (x > 9) && (y < 1) && (y > 9)) || (sudokubl[y - 1][x - 1] == 1));	//till a point of change is ok
 
-
-
-
-
+			printf("\n Enter value <1-9> : ");
+			scanf("%d", &value);
+		} while ((value < 1) && (value > 9) || ((sprawdzanie(sudoku, value, x, y, sudokubl)) == 1));	//till value is proper
+		save(sudoku);
+		system("CLS");
+	} while (finalcheck != 0);//till all values are different than 0
 
 	return 0;
 }
-
-
